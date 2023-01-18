554feb81ccf6533746897a750d7ce9eec98f552b
Goddoing <116724712+Goddoing@users.noreply.github.com>
2023-01-18 03:07:25 +0100

    Add files via upload

---
+++ js/image.js
@@ -0,0 +1,18 @@
+// const moreProductEl = document.querySelector(".more-product");
+
+// const btnEl = document.querySelector("btn");
+
+// btnEl.addEventListener("click", () => {
+//   ImageNum = 10;
+//   addNewImages();
+// });
+
+// function addNewImages() {
+//   for (let index = 0; index < imageNum; index++) {
+//     const newImgEl = document.createElement("img");
+//     newImgEl.src = `https://picsum.photos/300?random=${Math.floor(
+//       Math.random() * 2000
+//     )}`;
+//     imageContainerEl.appendChild(newImgEl);
+//   }
+// }
